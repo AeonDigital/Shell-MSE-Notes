61624b3efc876bc33e72bbe43011a63bff9eba56
Rianna Cantarelli <rianna.aeon@gmail.com>
2022-03-30 02:01:45 -0300

    Commit Inicial

---
+++ src/config/aliases.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# myShellEnv v 1.0 [aeondigital.com.br]
+
+
+
+
+
+
+
+alias freeNote="mse_notes_freeNote"
--- src/config/variables.sh
+++ src/config/variables.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 # myShellEnv v 1.0 [aeondigital.com.br]
+
+
+
+
+
+
+
+#
+# Armazena o nome de todos os módulos carregados.
+unset MSE_NOTES_FILE_CONTENT
+declare -a MSE_NOTES_FILE_CONTENT=()
